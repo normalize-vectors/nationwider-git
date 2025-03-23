7d8b25fff2a8075f195038e078ebb0ba384e85ef
Aurimukstis1 <71183486+Aurimukstis1@users.noreply.github.com>
2025-03-23 20:03:06 +0200

    updater executable added

--- updater.py
+++ updater.py
@@ -1,10 +1,23 @@
 import requests
-from os import getcwd
+url1= 'https://github.com/Aurimukstis1/nationwider-git/raw/refs/heads/main/dist/main.exe'
+url2= 'https://github.com/Aurimukstis1/nationwider-git/raw/refs/heads/main/dist/main-battle.exe'
 
-url = "https://raw.githubusercontent.com/Aurimukstis1/nationwider-git/main/dist/main.exe"
-directory = getcwd()
-filename = directory + 'somefile.txt'
-r = requests.get(url)
+response = requests.get(url1)
+file_Path = 'main.exe'
 
-f = open(filename,'w')
-f.write(r.content)
+if response.status_code == 200:
+    with open(file_Path, 'wb') as file:
+        file.write(response.content)
+    print('File : main.exe : downloaded successfully')
+else:
+    print('Failed to download file')
+
+response = requests.get(url2)
+file_Path = 'main-battle.exe'
+
+if response.status_code == 200:
+    with open(file_Path, 'wb') as file:
+        file.write(response.content)
+    print('File : main-battle.exe : downloaded successfully')
+else:
+    print('Failed to download file')
